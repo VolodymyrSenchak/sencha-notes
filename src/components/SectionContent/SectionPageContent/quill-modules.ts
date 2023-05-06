f13da42c3bc11914aaeec9a3a44b011f1f7ecc0f
Volodymyr Senchak <vova1993ua@gmail.com>
2023-05-06 10:37:19 +0300

    Add code highlight.

--- src/components/SectionContent/SectionPageContent/quill-modules.ts
+++ src/components/SectionContent/SectionPageContent/quill-modules.ts
@@ -1,3 +1,6 @@
+import highlight from "highlight.js";
+import "highlight.js/styles/monokai-sublime.css";
+
 export const QUILL_TOOLBAR_OPTIONS = [
   ["bold", "italic", "underline", "strike", "blockquote", "code-block"], // toggled buttons
   [{ list: "ordered" }, { list: "bullet" }, { header: [1, 2, 3, false] }],
@@ -6,6 +9,9 @@ export const QUILL_TOOLBAR_OPTIONS = [
 
 export const QUILL_MODULES = {
   toolbar: QUILL_TOOLBAR_OPTIONS,
+  syntax: {
+    highlight: (text: any) => highlight.highlightAuto(text).value
+  }, 
   clipboard: {
     matchVisual: false,
   }
